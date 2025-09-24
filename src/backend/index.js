2ba33a8ce06f21a71f2a48870f9272daf219716c
harain <jacksparrow-111111@mail.ru>
2025-09-25 00:37:42 +0300

    Добавил папку backend и сервер express. Реализовал CRUD методы для приложения TODOLIST

---
+++ src/backend/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+// const todos = require('./todos');
+const TODO_FILE = path.join(__dirname, 'todos.txt');
+
+
+function writeTodosToFile(todos) {
+    try {
+        fs.writeFileSync(TODO_FILE, JSON.stringify(todos, null, 2));
+        return true;
+    } catch (error) {
+        console.error('Ошибка при записи файла:', error);
+        return false;
+    }
+}
+
+const readFile = () => {
+    const data = fs.readFileSync(TODO_FILE, 'utf8');
+    return JSON.parse(data);
+}
+
+app.get('/todos', (req, res) => {
+    // Получаем весь список дел из файла
+    const todos = readFile();
+    res.send({ ok: true, data: JSON.stringify(todos) });
+});
+
+app.post('/todos', (req, res) => {
+    // Создаём новое дело, формат: {id:number, title:string}
+    const newTodo = req.body;
+    const todos = readFile();
+
+    todos.push({ id: todos.length + 1, title: newTodo.title });
+    writeTodosToFile(todos);
+
+    res.send({ ok: true, data: JSON.stringify(todos) })
+})
+
+app.patch('/todos', (req, res) => {
+    // Обновляем задачу формат: {id:number, title:string}
+    const editTodo = req.body;
+    const todos = readFile();
+    const newTodos = todos.map((todo) => (todo.id === editTodo.id) ? { ...todo, title: editTodo.title } : todo)
+    writeTodosToFile(newTodos);
+    res.send({ ok: true, data: JSON.stringify(newTodos) })
+})
+
+app.delete('/todos/:id', (req, res) => {
+    // Обновляем задачу формат: /todos/3 - id статьи
+    const deleteTodoId = req.params.id;
+    const todos = readFile();
+    const newTodos = todos.filter((todo)=>todo.id != deleteTodoId);
+    writeTodosToFile(newTodos);
+    res.send({ ok: true, data: JSON.stringify(newTodos) })
+})
+
+app.listen(3000)
+
+
+
+
+
